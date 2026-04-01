8f5b587f4ba99deeb9a46e101ff5e81089c4d2b5
saisjadhav1011 <saisjadhav1011@gmail.com>
2026-04-01 09:19:39 +0530

    added product entity

--- libs/database/src/migrations/index.ts
+++ libs/database/src/migrations/index.ts
@@ -1,7 +1,9 @@
 import { Migration1775014218437 } from "./1775014218437-migration";
 import { Migration1775014903670 } from "./1775014903670-migration";
+import { Migration1775015312463 } from "./1775015312463-migration";
 
 export const migrations = [
     Migration1775014218437,
     Migration1775014903670,
+    Migration1775015312463,
 ];
---
+++ libs/database/src/types.ts
@@ -0,0 +1,27 @@
+export enum UnitType {
+    COUNT = 'COUNT',     // pieces, boxes
+    WEIGHT = 'WEIGHT',   // kg, gram
+    VOLUME = 'VOLUME',   // liter, ml
+    LENGTH = 'LENGTH'    // meter
+}
+
+export enum ProductUnit {
+    // COUNT
+    PIECE = 'PIECE',
+    BOX = 'BOX',
+    PACK = 'PACK',
+    DOZEN = 'DOZEN',
+
+    // WEIGHT
+    KG = 'KG',
+    GRAM = 'GRAM',
+
+    // VOLUME
+    LITER = 'LITER',
+    ML = 'ML',
+
+    // LENGTH
+    METER = 'METER',
+    FEET = 'FEET',
+    INCH = 'INCH'
+}
