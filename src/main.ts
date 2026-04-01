841a50629cc918e11fe826ad45f2bd9db4d3dea3
saisjadhav1011 <saisjadhav1011@gmail.com>
2026-04-01 08:51:15 +0530

    added class validator and transformer

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // removes unwanted fields
+      forbidNonWhitelisted: true, // throws error for extra fields
+      transform: true, // auto converts types
+    }),
+  );
 
   if (process.env.NODE_ENV !== 'production') {
     setupSwagger(app);
